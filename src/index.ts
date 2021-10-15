82bc034eaa64b8c219a271b9394169b8be6b7e39
cassiomatoso <cassiocg98@gmail.com>
2021-10-14 21:52:58 -0300

    Implementando middlewares na index

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { itensRouter } from "./itens/itens.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -26,6 +28,8 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/menu/itens", itensRouter);
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 /**
  * Ativa server
